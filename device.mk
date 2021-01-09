ed2a349f80dcf67069dc085c81c31ef2920dc6f1
EmaMaker <emamaker0@gmail.com>
2021-01-09 23:19:34 +0100

    albus: build vendor.lineage.livedisplay@2.0-service-sysfs And add back livedisplay to the manifest

--- device.mk
+++ device.mk
@@ -262,7 +262,8 @@ PRODUCT_PACKAGES += \
 
 # Display Calibration
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.0-service-sysfs
 
 # DRM
 PRODUCT_PACKAGES += \
